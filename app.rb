41357fafd12188e3006282d323b4f29ee645a5a4
Quan Nguyen <quan.nguyen.anh89@gmail.com>
2017-08-31 09:18:04 +0700

    update blockchain info service

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require "sinatra/json"
 require './block_cypher'
+require './ether_scan'
 require './blockchain_info'
 require 'fast_blank'
 
@@ -9,13 +10,15 @@ get '/' do
 end
 
 get '/btc/:addr' do
-  # Time.parse(params[:at])
-  time = params[:at].nil? ? Time.parse("2000-01-01") : Time.parse(params[:at])
   balance = BlockchainInfo.btc_balance_at(params[:addr], time)
-  json balance: balance
+  json balance: balance, status: 1
 end
 
 get '/eth/:addr' do
-  balance = BlockCypher.eth_balance_at(params[:addr])
-  json balance: balance
+  balance = EtherScan.eth_balance_at(params[:addr], time)
+  json balance: balance, status: 1
+end
+
+def time
+  params[:at].nil? ? Time.now : Time.parse(params[:at])
 end
--- blockchain_info.rb
+++ blockchain_info.rb
@@ -1,23 +1,22 @@
 require 'httparty'
 require './constants'
-require 'fast_blank'
-require 'byebug'
 
 class BlockchainInfo
   include HTTParty
-  base_uri 'https://blockchain.info/'
+  base_uri 'https://blockchain.info'
+
   class << self
     def btc_balance_at(addr, time)
       time = time.to_i
       response = self.get("/rawaddr/#{addr}?limit=50").parsed_response
       n_tx = response["n_tx"]
-      page = n_tx / 50 
-      page += 1 if n_tx % 50 != 0
+      n_page = n_tx / 50 
+      n_page += 1 if n_tx % 50 != 0
       balance = 0
-      page.times do |page|
+      n_page.times do |page|
         next unless txs = response["txs"]
         balance += txs.reduce(0) do |memo, tx|
-          next if time > tx["time"]
+          next memo if tx["time"] > time  
           if tx["inputs"]
             tx["inputs"].each do |input|
               next unless prev_out = input["prev_out"]
@@ -29,10 +28,10 @@ class BlockchainInfo
             tx["out"].each do |o|
               memo += o["value"] if o["addr"] == addr
             end
-          end
-          response = self.get("/rawaddr/#{addr}?limit=50&offset=#{50 * (page+1)}").parsed_response
+          end          
           memo
         end
+        response = self.get("/rawaddr/#{addr}?limit=50&offset=#{50 * (page+1)}").parsed_response
       end
       balance / Constants::SATOSHI_PER_BTC
     end
